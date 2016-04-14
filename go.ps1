a58253e901f610784f07f7ccedc345f799bb031c
Jonathan.curtis <Jonathan.curtis@huddle.com>
2016-04-14 09:55:10 +0100

    fail build on team city

--- go.ps1
+++ go.ps1
@@ -11,3 +11,5 @@ Push-Location .\build
 Import-Module -Name ..\src\packages\psake*\tools\psake.psm1
 Invoke-psake -buildFile .\default.ps1 -taskList $task
 Pop-Location
+
+if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }
